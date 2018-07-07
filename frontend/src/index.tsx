397e19ce68d76d82e70a7feffc6e82c57686b851
sugyan <sugi1982@gmail.com>
2018-07-08 00:53:09 +0900

    Update directories

---
+++ frontend/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+window.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+        <div>
+            <p>Hello, world!</p>
+        </div>,
+        document.getElementById('main')
+    );
+});
