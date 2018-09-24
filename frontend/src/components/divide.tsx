19e47797bebb901030284ba3f056aa0cc632500b
sugyan <sugi1982@gmail.com>
2018-09-25 00:39:58 +0900

    Add modal dialog

--- frontend/src/components/divide.tsx
+++ frontend/src/components/divide.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import { connect } from "react-redux";
+import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
 import { Dispatch } from "redux";
 
 import { Action, changeDivideAction } from "../redux/actions";
 import { IdivideNums, Istate } from "../redux/reducers";
+
+interface Ilabel {
+    label?: string;
+    value?: string;
+}
 
 interface Iprops {
     size: number;
@@ -23,32 +29,92 @@ type Props = Iprops & IstateProps & IdispatchProps;
 
 interface IdivideState {
     images: string[];
+    modal: boolean;
+    targetImage: string;
 }
 
 class Divide extends React.Component<Props, IdivideState> {
+    private options: Ilabel[] = [
+        {},
+        { value: "BLANK", label: "空白" },
+        { value: "B_FU", label: "▲歩兵 (B_FU)" },
+        { value: "B_KY", label: "▲香車 (B_KY)" },
+        { value: "B_KE", label: "▲桂馬 (B_KE)" },
+        { value: "B_GI", label: "▲銀将 (B_GI)" },
+        { value: "B_KI", label: "▲金将 (B_KI)" },
+        { value: "B_KA", label: "▲角行 (B_KA)" },
+        { value: "B_HI", label: "▲飛車 (B_HI)" },
+        { value: "B_OU", label: "▲玉将 (B_OU)" },
+        { value: "B_TO", label: "▲と金 (B_TO)" },
+        { value: "B_NY", label: "▲成香 (B_NY)" },
+        { value: "B_NK", label: "▲成桂 (B_NK)" },
+        { value: "B_NG", label: "▲成銀 (B_NG)" },
+        { value: "B_UM", label: "▲竜馬 (B_UM)" },
+        { value: "B_RY", label: "▲竜王 (B_RY)" },
+        { value: "W_FU", label: "△歩兵 (W_FU)" },
+        { value: "W_KY", label: "△香車 (W_KY)" },
+        { value: "W_KE", label: "△桂馬 (W_KE)" },
+        { value: "W_GI", label: "△銀将 (W_GI)" },
+        { value: "W_KI", label: "△金将 (W_KI)" },
+        { value: "W_KA", label: "△角行 (W_KA)" },
+        { value: "W_HI", label: "△飛車 (W_HI)" },
+        { value: "W_OU", label: "△玉将 (W_OU)" },
+        { value: "W_TO", label: "△と金 (W_TO)" },
+        { value: "W_NY", label: "△成香 (W_NY)" },
+        { value: "W_NK", label: "△成桂 (W_NK)" },
+        { value: "W_NG", label: "△成銀 (W_NG)" },
+        { value: "W_UM", label: "△竜馬 (W_UM)" },
+        { value: "W_RY", label: "△竜王 (W_RY)" },
+    ];
     public constructor(props: Props) {
         super(props);
         this.state = {
             images: [],
+            modal: false,
+            targetImage: "",
         };
     }
     public render(): React.ReactNode {
         const { divide, image } = this.props;
-        const { images } = this.state;
+        const { images, modal, targetImage } = this.state;
         if (!image) {
             return null;
         }
         const results: React.ReactNode[] = images.map((v, i) => {
             return (
-                <tr key={i} style={{ marginBottom: 5 }}>
+                <tr key={i} style={{ marginBottom: 5 }} onClick={this.onClickImage.bind(this, i)}>
                   <td>
                     <img src={v} />
                   </td>
                 </tr>
             );
         });
+        const options = this.options.map((option: Ilabel, i: number) => {
+            return (
+                <option key={i} value={option.value}>{option.label}</option>
+            );
+        });
         return (
             <div>
+              <Modal isOpen={modal} toggle={this.toggleModal.bind(this)} backdrop={true}>
+                <ModalBody>
+                  <div className="row">
+                    <div className="col-md-3">
+                      <img src={targetImage} />
+                    </div>
+                    <div className="col-md-9">
+                      <select className="form-control">
+                        {options}
+                      </select>
+                    </div>
+                  </div>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="secondary" onClick={this.toggleModal.bind(this)}>
+                    Cancel
+                  </Button>
+                </ModalFooter>
+              </Modal>
               <form onSubmit={this.onSubmit.bind(this)}>
                 <div className="form-group row">
                   <label className="col-sm-3 col-form-label">Rows</label>
@@ -124,6 +190,17 @@ class Divide extends React.Component<Props, IdivideState> {
             }
         }
         this.setState({ images });
+    }
+    private onClickImage(i: number) {
+        const { images } = this.state;
+        this.setState({
+            modal: true,
+            targetImage: images[i],
+        });
+    }
+    private toggleModal() {
+        const { modal } = this.state;
+        this.setState({ modal: !modal });
     }
 }
 export default connect(
