c2cab72b6de49a6ac4525b30d47e32cdade72012
sugyan <sugi1982@gmail.com>
2018-10-28 16:56:40 +0900

    Update command to enable upload multiple files

--- cmd/upload/main.go
+++ cmd/upload/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"io/ioutil"
 	"log"
+	"path/filepath"
 
 	"github.com/sugyan/shogi-dataset/common"
 	"golang.org/x/oauth2/google"
@@ -14,12 +15,14 @@ import (
 var (
 	host  string
 	file  string
+	dir   string
 	label string
 )
 
 func init() {
 	flag.StringVar(&host, "host", "localhost:8080", "hostname of Remote API")
 	flag.StringVar(&file, "file", "./example.jpg", "filename to upload")
+	flag.StringVar(&dir, "dir", "", "search directory for uploading")
 	flag.StringVar(&label, "label", "", "label name of image")
 }
 
@@ -29,26 +32,47 @@ func main() {
 		log.Fatal("label is empty")
 	}
 
-	data, err := ioutil.ReadFile(file)
-	if err != nil {
+	files := []string{}
+	if dir != "" {
+		matches, err := filepath.Glob(filepath.Join(dir, "*.jpg"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, match := range matches {
+			files = append(files, match)
+		}
+	} else {
+		files = append(files, file)
+	}
+	if err := upload(files, label); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func upload(files []string, label string) error {
 	client, err := google.DefaultClient(context.Background(),
 		"https://www.googleapis.com/auth/appengine.apis",
 		"https://www.googleapis.com/auth/userinfo.email",
 		"https://www.googleapis.com/auth/cloud-platform",
 	)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	ctx, err := remote_api.NewRemoteContext(host, client)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	key, err := common.RegisterImage(ctx, data, label)
-	if err != nil {
-		log.Fatal(err)
+	for _, file := range files {
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			return err
+		}
+		key, err := common.RegisterImage(ctx, data, label)
+		if err != nil {
+			return err
+		}
+		log.Printf("%v saved", key.Encode())
 	}
-	log.Printf("%v saved", key.Encode())
+	return nil
 }
