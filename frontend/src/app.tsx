032f5f6cadcda60b8115fcd159d6b35d2ef79dc9
sugyan <sugi1982@gmail.com>
2019-01-02 22:28:36 +0900

    Add navbar

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -8,19 +8,16 @@ import Index from "./apps/index";
 import Label from "./apps/label";
 import Login from "./apps/login";
 import Upload from "./apps/upload";
+import Navbar from "./components/navbar";
 import { CommonAction, setUserAction } from "./redux/actions/common";
 import { Istate } from "./redux/reducer";
 import { Iuser, userRole } from "./redux/reducers/common";
-
-interface IstateProps {
-    user?: Iuser;
-}
 
 interface IdispatchProps {
     setUser: (user: Iuser) => CommonAction;
 }
 
-type Props = IstateProps & IdispatchProps;
+type Props = IdispatchProps;
 
 class App extends React.Component<Props> {
     public componentDidMount() {
@@ -38,51 +35,24 @@ class App extends React.Component<Props> {
         });
     }
     public render() {
-        const { user } = this.props;
-        let editorMenu: React.ReactNode;
-        if (user && user.role === userRole.editor) {
-            editorMenu = (
-                <React.Fragment>
-                  <li className="nav-item">
-                    <Link to="/upload" className="nav-link">Upload</Link>
-                  </li>
-                </React.Fragment>
-            );
-        }
         return (
             <BrowserRouter>
               <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                  <div className="container">
-                    <Link to="/" className="navbar-brand">Shogi Dataset</Link>
-                    <div className="collapse navbar-collapse">
-                      <ul className="navbar-nav">
-                        {editorMenu}
-                      </ul>
-                    </div>
-                  </div>
-                </nav>
+                <Navbar />
                 <div className="container py-md-3">
-                  <div>
-                    <Route exact path="/" component={Index} />
-                    <Route exact path="/login" component={Login} />
-                    <Route path="/image" component={Image} />
-                    <Route path="/label/:label" component={Label} />
-                    <Route exact path="/upload" component={Upload} />
-                  </div>
+                  <Route exact path="/" component={Index} />
+                  <Route exact path="/login" component={Login} />
+                  <Route path="/image" component={Image} />
+                  <Route path="/label/:label" component={Label} />
+                  <Route exact path="/upload" component={Upload} />
                 </div>
               </div>
             </BrowserRouter>
         );
     }
 }
-
 export default connect(
-    (state: Istate): IstateProps => {
-        return {
-            user: state.commonReducer.user,
-        };
-    },
+    (state: Istate) => state,
     (dispatch: Dispatch): IdispatchProps => {
         return {
             setUser: (user: Iuser): CommonAction => dispatch(setUserAction(user)),
