d71db359e8efb2a68cf13199e2ad750fb35a92af
sugyan <sugi1982@gmail.com>
2019-07-27 00:41:02 +0900

    Use create-react-app

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,63 +1,26 @@
-import * as React from "react";
-import { connect } from "react-redux";
-import { BrowserRouter, Route } from "react-router-dom";
-import { Dispatch } from "redux";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Api from "./apps/api";
-import Image from "./apps/image";
-import Index from "./apps/index";
-import Label from "./apps/label";
-import Login from "./apps/login";
-import Upload from "./apps/upload";
-import Navbar from "./components/navbar";
-import { CommonAction, setUserAction } from "./redux/actions/common";
-import { Istate } from "./redux/reducer";
-import { Iuser, userRole } from "./redux/reducers/common";
-
-interface IdispatchProps {
-    setUser: (user: Iuser) => CommonAction;
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React!!!!
+        </a>
+      </header>
+    </div>
+  );
 }
 
-type Props = IdispatchProps;
-
-class App extends React.Component<Props> {
-    public componentDidMount() {
-        const { setUser } = this.props;
-        fetch(
-            "/api/user",
-        ).then((res: Response) => {
-            return res.json();
-        }).then((user: Iuser) => {
-            if (user.name && user.role) {
-                setUser(user);
-            } else {
-                setUser({ role: userRole.anonymous });
-            }
-        });
-    }
-    public render() {
-        return (
-            <BrowserRouter>
-              <div>
-                <Navbar />
-                <div className="container py-md-3">
-                  <Route exact path="/" component={Index} />
-                  <Route exact path="/api" component={Api} />
-                  <Route exact path="/login" component={Login} />
-                  <Route path="/image" component={Image} />
-                  <Route path="/label/:label" component={Label} />
-                  <Route exact path="/upload" component={Upload} />
-                </div>
-              </div>
-            </BrowserRouter>
-        );
-    }
-}
-export default connect(
-    (state: Istate) => state,
-    (dispatch: Dispatch): IdispatchProps => {
-        return {
-            setUser: (user: Iuser): CommonAction => dispatch(setUserAction(user)),
-        };
-    },
-)(App);
+export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,12 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, Store } from "redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import App from "./app";
-import { reducer } from "./redux/reducer";
+ReactDOM.render(<App />, document.getElementById('root'));
 
-window.addEventListener("DOMContentLoaded", () => {
-    const store: Store = createStore(reducer);
-    ReactDOM.render(
-        <Provider store={store}><App /></Provider>,
-        document.getElementById("app"),
-    );
-});
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
