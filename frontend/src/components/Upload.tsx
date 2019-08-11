5f57ea9cd11321b0330b1d38921aa2c4fece75fe
sugyan <sugi1982@gmail.com>
2019-08-11 22:27:15 +0900

    frontend: Update Upload to hooks

--- frontend/src/components/Upload.tsx
+++ frontend/src/components/Upload.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch } from "react";
+import React, { Dispatch, useEffect } from "react";
 import { connect } from "react-redux";
 import { RouteComponentProps, withRouter } from "react-router";
 import { Row, Col } from "reactstrap";
@@ -7,7 +7,7 @@ import Canvas from "./upload/Canvas";
 import Perspective from "./upload/Perspective";
 import Divide from "./upload/Divide";
 import { AppState } from "../redux/store";
-import { UploadAction, loadImage, updatePoints } from "../redux/actions";
+import { UploadAction, loadImage, updatePoints, updateImageData } from "../redux/actions";
 import { User, UserRole } from "../redux/reducers";
 
 interface StateProps {
@@ -16,44 +16,42 @@ interface StateProps {
 
 interface DispatchProps {
     loadImage: (image?: HTMLImageElement) => void;
+    updateImageData: (imageData?: ImageData) => void;
     updatePoints: (points: number[]) => void;
 }
 
 type Props = StateProps & DispatchProps & RouteComponentProps<{}>;
 
-class Upload extends React.Component<Props> {
-    public componentWillMount(): void {
-        const { user, history } = this.props;
-        if (user && user.role !== UserRole.editor) {
-            history.push("/");
-        }
+const Upload: React.FC<Props> = (props: Props): JSX.Element | null => {
+    const { user, history } = props;
+
+    useEffect((): () => void => {
+        return (): void => {
+            const { loadImage, updateImageData, updatePoints } = props;
+            loadImage(undefined);
+            updateImageData(undefined);
+            updatePoints([]);
+        };
+    });
+
+    if (user && user.role !== UserRole.editor) {
+        history.push("/");
+        return null;
     }
-    public componentWillReceiveProps(props: Props): void {
-        const { user, history } = props;
-        if (user && user.role !== UserRole.editor) {
-            history.push("/");
-        }
-    }
-    public componentWillUnmount(): void {
-        const { loadImage, updatePoints } = this.props;
-        loadImage(undefined);
-        updatePoints([]);
-    }
-    public render(): JSX.Element {
-        return (
-          <Row>
-            <Col xs="8">
-              <Canvas size={1024} />
-            </Col>
-            <Col xs="4">
-              <Perspective size={96 * 9} />
-              <hr />
-              <Divide size={96} />
-            </Col>
-          </Row>
-        );
-    }
-}
+
+    return (
+      <Row>
+        <Col xs="8">
+          <Canvas size={1024} />
+        </Col>
+        <Col xs="4">
+          <Perspective size={96 * 9} />
+          <hr />
+          <Divide size={96} />
+        </Col>
+      </Row>
+    );
+};
 
 export default withRouter(connect(
     (state: AppState): StateProps => {
@@ -65,6 +63,9 @@ export default withRouter(connect(
         return {
             loadImage: (image?: HTMLImageElement): void => {
                 dispatch(loadImage(image));
+            },
+            updateImageData: (imageData?: ImageData): void => {
+                dispatch(updateImageData(imageData));
             },
             updatePoints: (points: number[]): void => {
                 dispatch(updatePoints(points));
